c3a85d1905c68d35c3745633a3aba7367f8b71bc
Leonardo Braga <programer_frontbraga@outlook.com>
2024-09-01 00:25:42 -0300

    chore: Path das imagens alterada, pois o github pages não estava renderizando.

--- src/assets/scripts/data.ts
+++ src/assets/scripts/data.ts
@@ -1,16 +1,16 @@
 export const dayPeriodImages = {
-  nightImg: '/src/assets/images/night.jpg',
-  afternoonImg: '/src/assets/images/afternoon.jpg',
-  noonImg: '/src/assets/images/noon.jpg',
-  middleDayImg: '/src/assets/images/middleDay.jpg',
-  lateMorningImg: '/src/assets/images/lateMorning.jpg',
-  morningImg: '/src/assets/images/morning.jpg',
-  midnightImg: '/src/assets/images/midnight.jpg',
+  nightImg: './src/assets/images/night.jpg',
+  afternoonImg: './src/assets/images/afternoon.jpg',
+  noonImg: './src/assets/images/noon.jpg',
+  middleDayImg: './src/assets/images/middleDay.jpg',
+  lateMorningImg: './src/assets/images/lateMorning.jpg',
+  morningImg: './src/assets/images/morning.jpg',
+  midnightImg: './src/assets/images/midnight.jpg',
 };
 
 export const seasonImages = {
-  springImg: '/src/assets/images/spring.png',
-  summerImg: '/src/assets/images/summer.png',
-  fallImg: '/src/assets/images/fall.png',
-  winterImg: '/src/assets/images/winter.png',
+  springImg: './src/assets/images/spring.png',
+  summerImg: './src/assets/images/summer.png',
+  fallImg: './src/assets/images/fall.png',
+  winterImg: './src/assets/images/winter.png',
 };
